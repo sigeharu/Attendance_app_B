882e46ed01cb5d90fd31ca6a6b7f9485bb46f094
sigeharu <sigeharumiyamoto@gmail.com>
2020-06-24 23:58:46 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def full_title(pgaePname = "")
+      base_title = "AttendanceAppB"
+      if page_name.empty?
+        base_title
+      else
+        page_name + " | " + base_title
+      end
+  end
 end
